1dbdf97e58188147871bfd0cc29e82c7685f6087
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-08-10 22:51:25 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is my attempt at making a live image recognition software. I will use tensorflow and keras imports to achieve this. 
+
+Install: 
+pip insall tensorflow
---
+++ main.py
@@ -0,0 +1,44 @@
+import tensorflow as tf    
+
+import os
+from pathlib import Path
+
+script_dir = Path(__file__).resolve().parent
+
+file_name = (
+    / "TRAIN_VAL"
+)
+
+training_dir = (
+    / "TRAIN_VAL"
+    / "TRAINING"
+)
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu' ,
+                input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(learning_rate=0.001),
+              metrics=['accuracy'])
+
+history = model.fit(
+    train_generator,
+    epochs=1 # change this for train epochs number
+)
